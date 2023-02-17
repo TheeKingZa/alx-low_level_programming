2a55b9774bd8937a4e2e6127b20b8deeed434c97
Pule <proceedingmc@gmail.com>
2023-02-17 06:25:32 -0800

    edited

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int master(void)
 	print("Size of char:%d byte(s)\n", sizeof(char));
 	print("Size of int:%d byte(s)\n", sizeof(int));
 	print("Size of long int:%d byte(s)\n", sizeof(long int));
-	print("Size of long long int:%d byte(s)\n" sizeof(long long int));
+	print("Size of long long int:%d byte(s)\n", sizeof(long long int));
 	print("Size of float:%d byte(s)\n", sizeof(float));
 	return (0);
 }

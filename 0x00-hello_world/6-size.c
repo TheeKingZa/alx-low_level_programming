d413c31a69ac6eab12216495b81ceb8fc5062d35
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-02-17 14:23:54 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,4 @@ printf("Size of long int: %d byte(s)\n", sizeof(long int));
 printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of float: %d byte(s)\n", sizeof(float));
 return(0);
- 
 }

ba3d3166cf684793d90973aec2a63dc6dfc6d40f
Pule <proceedingmc@gmail.com>
2023-02-17 03:18:06 -0800

    Piece of Art

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Using printf function
+ *
+ * Return - 0
+ */
+
+int main(void)
+{
+ printf("with proper grammer, but the outcome is a piece of art,\n");
+ return (0);
+
+
+}

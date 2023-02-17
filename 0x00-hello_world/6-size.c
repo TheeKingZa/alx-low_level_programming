6a0f611598b21c41a6fd1129b44576d7cdfa9a69
Pule <proceedingmc@gmail.com>
2023-02-17 05:55:05 -0800

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-/**
+/*
  *
  * main - prints various types of sizes of a computer.
  *
- * return 0
+ * return:  0
  */
 
-int main(void){
-
+int main(void)
+{
 	print("Size of char:%d byte(s)\n", sizeof(char));
 	print("Size of int:%d byte(s)\n", sizeof(int));
 	print("Size of long int:%d byte(s)\n", sizeof(long int);
 	print("Size of long long int:%d byte(s)\n" sizeof(long long int);
 	print("Size of float:%d byte(s)\n", sizeof(float));
 	return (0);
+
+	main "$@"
 }

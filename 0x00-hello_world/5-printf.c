f79518416b822aa336586b37030f172c5b1900fd
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-02-17 13:50:56 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
- * main -Using printf function
- *
- * Return - 0
- */
-
+* main -Using printf function
+*
+* Return -0
+*/
 int main(void)
 {
- printf("with proper grammer, but the outcome is a piece of art,\n");
- return (0);
-
-
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }

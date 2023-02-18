724b04ff91ac193ede90fa0c1d5e9b04c977838a
Pule <proceedingmc@gmail.com>
2023-02-18 07:56:23 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * main -prints sizesof()
+ *
+ * return : 0 always.(success)
+ */
+int main()
+{
+  printf("The size of int is %lu bytes\n", sizeof(int));
+  printf("The size of short is %lu bytes\n", sizeof(short));
+  printf("The size of long is %lu bytes\n", sizeof(long));
+  printf("The size of long long is %lu bytes\n", sizeof(long long));
+  printf("The size of char is %lu bytes\n", sizeof(char));
+  printf("The size of float is %lu bytes\n", sizeof(float));
+  printf("The size of double is %lu bytes\n", sizeof(double));
+  printf("The size of long double is %lu bytes\n", sizeof(long double));
+  printf("The size of void * is %lu bytes\n", sizeof(void *));
+  
+  /*return 0 for success*/
+
+  return 0;
+}

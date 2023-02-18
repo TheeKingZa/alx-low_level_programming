cec542d483b2fb16a9cd549edd6bf731a4fef52b
Pule <proceedingmc@gmail.com>
2023-02-18 06:09:51 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+/**
+ * Main - Puts funtion to print message.
+ *
+ * Return 0 always
+ */
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle");
+return (0);
+}
+
+/* Returns 0 to indicate successful execution. */

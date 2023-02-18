adc50ee61f5c465e2634c880fdca48c7f8af1138
Pule <proceedingmc@gmail.com>
2023-02-18 09:19:42 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 /*
- * main -running on sizeof 32bit or 64bit.
+ * Main -Sizesof
  *
- * return (0) Always(success)
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("Checking system type...\n");
-	/*Check if running on 32-bit or 64-bit system*/
-
-	if (sizeof(*void) == 4)
-	{
-		printf("Running on a 32-bit system.\n");
-		printf("The size of char is %u\n", sizeof(char));
-		printf("The size of int is %u\n", sizeof(int));
-		printf("The size of long int is %u\n", sizeof(long int));
-		printf("The size of long long int is %u\n", sizeof(long long int))
-		printf("The size of float is %u\n", sizeof(float));
-}
-	else
-{
-		printf("Running on a 64-bit system.\n");
-		printf("The size of char is %lu\n", sizeof(char));
-		printf("The size of int is %lu\n", sizeof(int));
-		printf("The size of long is int %lu\n", sizeof(long int));
-		printf("The size of long long int is %lu\n", sizeof(long long int));
-		printf("The size of float is %lu\n", sizeof(float));
-}
-/*return 0 for success*/
-		return (0);
+printf("Size of a char:%lu byte(s)\n", sizeof(char));
+printf("Size of an int:%lu byte(s)\n", sizeof(int));
+printf("Size of a long int:%lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int:%lu byte(s)\n", sizeof(long long int));
+printf("Size of a float:%lu byte(s)\n", sizeof(float));
+return (0);
 }

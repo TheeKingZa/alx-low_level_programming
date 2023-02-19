9cc720e2019e9efb4045b45029ba9418fa816d68
Pule <proceedingmc@gmail.com>
2023-02-19 07:11:05 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
-* main - Print using Puts.
-*
-* this program prints using puts,
-* ""Programming is like building a multilingual puzzle"
-*
-* Return : 0 Always.
-*/
+ * main - Entry point of program
+ *
+ * Description: Prints using 'puts' function
+ *
+ * Return: 0 (success code)
+ */
 int main(void)
 {
 				puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
-/*return 0 for successfull executable*/
+				return (0);
+/*return 0 success*/
 }

e4bd64351ef5320d71829ca142df121190bd7a62
Pule <proceedingmc@gmail.com>
2023-02-19 07:41:58 -0800

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,11 @@
  *
  * Description: Prints standard message using the `print` function
  *
- * Return: 0 (error code)
+ * Return: 0 (Success code)
  */
 int main(void)
 {
 				printf("with proper grammar, but the outcome is a piece of art,\n");
 				return (0);
+/*Return 0 success code*/
 }

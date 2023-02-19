801e26343424695aede702397619c74d90aff920
Pule <proceedingmc@gmail.com>
2023-02-19 07:36:00 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/*
-*
-* main - Printf function.
-*
-* return 0 Always (success)
-*
-*/
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints standard message using the `print` function
+ *
+ * Return: 0 (error code)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-
-/*Return 0 to prove success*/
+				printf("with proper grammar, but the outcome is a piece of art,\n");
+				return (0);
 }

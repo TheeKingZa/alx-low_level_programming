d490bbfe326393cb057325fa7bd8493e4fbd40c7
Pule <proceedingmc@gmail.com>
2023-02-19 06:58:38 -0800

    quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
 * main - Print using Puts.
 *
-* return : 0 Always.
+* this program prints using puts,
+* ""Programming is like building a multilingual puzzle"
+*
+* Return : 0 Always.
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+				puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 
 /*return 0 for successfull executable*/
 }

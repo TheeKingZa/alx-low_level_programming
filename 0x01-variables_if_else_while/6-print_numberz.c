dd575ede372917996f8ba17126ff538a9f2d1c3b
Pule <proceedingmc@gmail.com>
2023-02-20 00:45:44 -0800

    Each Char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry Point of program
+ *
+ * Description: Putchar 'print' numbers
+ *
+ * Return: 0 (Success code)
+ */
+int main(void)
+{
+				/*No declaration for this*/
+				putchar('0');
+				putchar('1');
+				putchar('2');
+				putchar('3');
+				putchar('4');
+				putchar('5');
+				putchar('6');
+				putchar('7');
+				putchar('8');
+				putchar('9');
+				putchar('\n');
+				/*Putchar print every number one by one*/
+				return (0);
+/*Return 0 (Success code)*/
+}

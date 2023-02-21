0c2a885595ce2e6ac99e9c8020ab56c4ab96f0fd
Pule <proceedingmc@gmail.com>
2023-02-21 04:59:46 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* main - Entry point
+*
+* Description: This program prints the text "_putchar" followed by a newline
+* character. It then returns 0 to indicate successful completion.
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+/* use _putchar to print the characters */
+				putchar('_');
+				putchar('p');
+				putchar('u');
+				putchar('t');
+				putchar('c');
+				putchar('h');
+				putchar('a');
+				putchar('r');
+				putchar('\n');
+/*Return 0 Success code*/
+				return (0);
+}

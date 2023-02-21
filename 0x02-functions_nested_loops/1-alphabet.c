5a26be6021938fd1fead27e04cc65f2d8e9d492a
Pule <proceedingmc@gmail.com>
2023-02-21 12:23:07 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*
+* main - Entry point of program
+*
+* Description: function that prints lowercase.
+*
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+				char letter = 'a';
+				while (letter <= 'z')
+				{
+				_putchar(letter);
+				letter++;
+				}
+				/*return void*/
+				_putchar('\n');
+}

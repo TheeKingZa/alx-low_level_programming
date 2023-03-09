efb315df6ea46e37931e533f878c87d0ad031c2b
Pule <proceedingmc@gmail.com>
2023-03-08 17:50:35 -0800

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,55 +1,31 @@
 #include "main.h"
+#include "holberton.h"
+
 /**
- * wildcmp - compares two strings and returns 1 if they can be considered identical
- *           otherwise returns 0. s2 can contain the special character * which can
- *           replace any string (including an empty string).
+ * wildcmp - Compares two strings, considering the wildcard character *.
+ * @s1: Pointer to the first string.
+ * @s2: Pointer to the second string.
  *
- * @s1: the first string to compare
- * @s2: the second string to compare
- *
- * Return: 1 if the strings can be considered identical, otherwise returns 0.
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-    {
-        /* Both strings are empty, so they are identical */
-        return 1;
-    }
-    else if (*s2 == '*')
-    {
-        /* Handle the case where the second string starts with a * character */
-        if (*s1 == '\0')
-        {
-            /* If the first string is empty and the second string consists only of * characters, they are identical */
-            while (*s2 == '*')
-            {
-                s2++;
-            }
-            return *s2 == '\0';
-        }
-        else
-        {
-            /* Otherwise, we can consider all possible suffixes of the first string and check if they match the remainder of the second string */
-            while (*s1 != '\0')
-            {
-                if (wildcmp(s1, s2 + 1))
-                {
-                    return 1;
-                }
-                s1++;
-            }
-            return wildcmp(s1, s2 + 1);
-        }
-    }
-    else if (*s1 != '\0' && (*s1 == *s2 || *s2 == '?'))
-    {
-        /* If the current characters in both strings match, or if the current character in the second string is a ?, continue comparing */
-        return wildcmp(s1 + 1, s2 + 1);
-    }
-    else
-    {
-        /* Otherwise, the strings are not identical */
-        return 0;
-    }
+if (*s1 == '\0' && *s2 == '\0') /* Base case: end of both strings */
+return (1);
+
+if (*s2 == '*')
+{
+if (*s1 == '\0') /* Case where * replaces an empty string */
+return (wildcmp(s1, s2 + 1));
+
+if (wildcmp(s1 + 1, s2) == 1) /* Check if * matches current character */
+return (1);
+
+return (wildcmp(s1, s2 + 1)); /* Check if * matches an empty string */
+}
+
+if (*s1 == *s2) /* If current characters match, move on to next */
+return (wildcmp(s1 + 1, s2 + 1));
+
+return (0); /* Strings are not identical */
 }

50fcf6258e264f04e5afb1b61f495d4a390d54d1
Pule <proceedingmc@gmail.com>
2023-03-08 18:20:30 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,15 @@
-#include "main.h"
-#include <stdio.h>
-#include <string.h>
-/**
- * is_palindrome - checks if a string is a palindrome
- *
- * @s: the string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-int i, j;
-int n = strlen(s);
+#!/usr/bin/perl -w
 
-				/* Iterate over the string from both ends */
-for (i = 0, j = n - 1; i < j; i++, j--)
-{
-				/**
-				 * If the characters at any position do not match
-				 * &the string is not a palindrome.
-				 */
-if (s[i] != s[j])
-{
-return (0);
+use strict;
+
+sub is_palindrome {
+    my () = @_;
+     = lc();  # convert to lowercase
+     =~ s/\W//g;  # remove non-alphanumeric characters
+    return ( eq reverse()) ? 1 : 0;
 }
-}				/**
-				 * If the iteration completes without finding any mismatch
-				 * &the string is a palindrome.
-				 */
-return (1);
-}
+
+# test the function
+print is_palindrome("A man, a plan, a canal, Panama!") . "\n";  # prints 1
+print is_palindrome("Not a palindrome") . "\n";  # prints 0
+print is_palindrome("") . "\n";  # prints 1

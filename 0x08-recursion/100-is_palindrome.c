f6ea304b47d06301e294f0fdb40e126d079d936c
Pule <proceedingmc@gmail.com>
2023-03-08 18:27:18 -0800

    7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,15 +1,29 @@
-#!/usr/bin/perl -w
+#include "main.h"
 
-use strict;
+/**
+ * wildcmp - compares two strings and returns 1 if they can be considered identical
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 1 if strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+/* base case: both strings are empty */
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
 
-sub is_palindrome {
-    my () = @_;
-     = lc();  # convert to lowercase
-     =~ s/\W//g;  # remove non-alphanumeric characters
-    return ( eq reverse()) ? 1 : 0;
+/* special case: * in s2 matches any string */
+if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
+
+/* case: current characters match, continue comparison */
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+
+/* case: current characters don't match, but * in s2 can replace it */
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+return (0);
 }
-
-# test the function
-print is_palindrome("A man, a plan, a canal, Panama!") . "\n";  # prints 1
-print is_palindrome("Not a palindrome") . "\n";  # prints 0
-print is_palindrome("") . "\n";  # prints 1

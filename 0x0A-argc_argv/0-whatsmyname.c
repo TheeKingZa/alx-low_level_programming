5b4c231bb5601af64cbce3ee60d5a778a68f1afb
Pule <proceedingmc@gmail.com>
2023-03-10 10:16:26 -0800

    num1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+				/**
+				 * Use printf to print the name of the
+				 * current file followed by a new line
+				 */
+printf("%s\n", __FILE__);
+
+/* Return 0 to indicate that the program has completed successfully */
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,12 @@
+0x0A. C - argc, argv
+
+CONTENT
+
+0-whatsmyname.c
+1-args.c
+2-args.c
+3-mul.c
+4-add.c
+------------
+1 Advanced Task
+------------

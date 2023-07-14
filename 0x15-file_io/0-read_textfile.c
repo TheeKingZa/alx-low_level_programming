469b0dd42f57947a6f141910f5f8c6c5a1e1e168
Pule <proceedingmc@gmail.com>
2023-07-14 11:05:07 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,50 @@
-#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * read_textfile - Reads a text file and prints
+ *	it to the POSIX standard output.
  *
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * @filename: The name of the text file to be read.
+ * @letters: The number of letters to read and print.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: The actual number of letters read and printed, or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fd;/* File descriptor */
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
-		return (0);
-
+	return (0);/* Open the file in read-only mode */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);/* Allocate memory for the buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
-
-	free(buffer);
-	close(o);
-
-	return (w);
+/* Read from the file */
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+/* Write to standard output */
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (bytes_written);
 }

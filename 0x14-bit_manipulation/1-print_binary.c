9532fabcc964f7680c6f0400398e30f4b90d668c
Pule <proceedingmc@gmail.com>
2023-07-14 03:17:22 -0700

    betty checks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,18 +12,18 @@ void print_binary(unsigned long int n)
 	for (i = 63; i >= 0; i--)
 	{
 		current = n >> i;
-	
+
 		if (current & 1)
 	{
 		_putchar('1');
 		count++;
 	}
-	
+
 	else if (count)
-		
+
 		_putchar('0');
 	}
-	
+
 	if (!count)
 	_putchar('0');
 }

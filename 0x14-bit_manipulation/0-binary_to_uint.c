66655e885c6ec5017ba73f14d16f8e54a039ddc1
Pule <proceedingmc@gmail.com>
2023-07-14 03:08:39 -0700

    lets try to revert to commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,26 +9,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-<<<<<<< HEAD
-	int i;
-	unsigned int dec_val = 0;
-=======
+
+
 int i;
 unsigned int dec_val=0;
->>>>>>> b84b2fc62fc19ffad2310930ca1242e77495c574
+
 
 if (!b)
 return (0);
 
-<<<<<<< HEAD
-	for (; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
 
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-=======
+for (; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+
+dec_val = 2 * dec_val + (b[i] - '0');
+}
+
 for (; b[i]; i++)
 {
 if(b[i] < '0' || b[i] > '1')
@@ -36,6 +34,5 @@ return (0);
 
 dec_val= 2 * dec_val + (b[i]-'0');
 }
->>>>>>> b84b2fc62fc19ffad2310930ca1242e77495c574
 return (dec_val);
 }

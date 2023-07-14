1d5632132fdb2202220ad134c0117daebe954289
Pule <proceedingmc@gmail.com>
2023-07-14 03:13:36 -0700

    0-Bin_to_in

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,11 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-
 int i;
 unsigned int dec_val=0;
 
-
 if (!b)
 return (0);
-
 
 for (; b[i]; i++)
 {
@@ -27,12 +23,5 @@ return (0);
 dec_val = 2 * dec_val + (b[i] - '0');
 }
 
-for (; b[i]; i++)
-{
-if(b[i] < '0' || b[i] > '1')
-return (0);
-
-dec_val= 2 * dec_val + (b[i]-'0');
-}
 return (dec_val);
 }

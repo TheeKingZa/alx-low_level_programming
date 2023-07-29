e7b73ef18916faaad8f30900767320768af70fa2
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-07-29 11:20:12 +0200

    Update main.h
    _putchar and Cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+int _putchar(char c);
+int copy_files(const char *file_from, const char *file_to);
 
 
 

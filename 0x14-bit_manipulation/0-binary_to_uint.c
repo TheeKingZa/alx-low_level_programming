c8990ed5ae1284c4c9387f07d43657c6a72a09a6
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-07-29 11:46:26 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to unsigned int
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
  *
- * @b: string contain binary number
- *
- * Return: converted number
+ * Return: the converted value
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int dec_value = 0;
+unsigned int decimal = 0;
 int str_len = 0, base = 1;
 
 if (!check_valid_string(b))
@@ -20,21 +18,20 @@ str_len++;
 
 while (str_len)
 {
-dec_value += ((b[str_len - 1] - '0') * base);
+decimal += ((b[str_len - 1] - '0') * base);
 base *= 2;
 str_len--;
 }
-return (dec_value);
+return (decimal);
 }
 
 /**
- * chck_valid_str - checks a string if it has only 0's and 1's
- * 
+ * check_valid_string - checks if a string has only 0's and 1's
  * @b: string to be checked
  *
  * Return: 1 if string is valid, 0 otherwise
  */
-int chck_valid_str(const char *b)
+int check_valid_string(const char *b)
 {
 if (b == NULL)
 return (0);

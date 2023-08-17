a492e72f2b784fc5156fcb77003c62df792f4519
Pule <proceedingmc@gmail.com>
2023-08-17 23:23:05 +0200

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,10 +14,6 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	/* Initialize the node count */
 
-	/* Traverse the list and print each element */
-
-	/* dlistint_t *h = head; */
-	/* Assuming head is a global variable */
 
 	while (h != NULL)
 		{

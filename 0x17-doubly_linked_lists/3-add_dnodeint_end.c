46133736a1a618889c3f1bbd070fd86e67476cb3
Pule <proceedingmc@gmail.com>
2023-08-18 02:32:13 +0200

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,45 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: Pointer to a pointer to the head of the dlistint_t list.
- * @n: Value to be assigned to the new node.
+ *  add_dnodeint_end -  adds a new node at the end of a dlistint_t list.
+ * @head: double pointer to the head of the list
+ * @n: integer to add to the new node
  *
- * Return: The address of the new element, or NULL if it failed.
+ * Return:  the address of the new element, or NULL if it failed
  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	/* Allocate memory for the new node */
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+        dlistint_t *new_node;
 
-	if (new_node == NULL)
-		return (NULL);
-	/* Allocation failed */
+        new_node = malloc(sizeof(dlistint_t));
 
-	/* Initialize the new node */
-	new_node->n = n;
-	new_node->next = NULL;
+        if (new_node == NULL)
+                return (NULL);
 
-	/* If the list is empty, update the head to point to the new node */
-	if (*head == NULL)
-	{
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
-	}
+        new_node->n = n;
+        new_node->next = NULL;
 
-	/* Traverse the list to find the last node */
-	dlistint_t *current = *head;
+        if (*head == NULL)
+        {
+                new_node->prev = NULL;
+                *head = new_node;
+                return (new_node);
+        }
 
-	while (current->next != NULL)
-	{
-		current = current->next;
-	}
-/* Update pointers to add the new node at the end */
-new_node->prev = current;
-current->next = new_node;
+        while ((*head)->next != NULL)
+                *head = (*head)->next;
 
-return (new_node);
+        (*head)->next = new_node;
+        new_node->prev = *head;
+
+        return (new_node);
 }

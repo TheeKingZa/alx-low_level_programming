e5d6d6b1a9d035452fae359dcf2ef90d19e410a9
Pule <proceedingmc@gmail.com>
2023-08-25 11:04:28 +0200

    dynamic

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 # Compile C source files with the Betty style
-gcc -Wall -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 
 # Create the shared library
 gcc -shared *.o -o liball.so
+
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH

ac6af17cdeacbdc167674abeae94e147605396b0
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-10-25 20:13:36 +0200

    Update 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    island_perimeter - Calculate the perimeter of the island.
+
+    :param grid: List of lists representing the island grid.
+    :return: The perimeter of the island.
+    """
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # If the current cell is land, add 4 to the perimeter (all sides).
+                perimeter += 4
+
+                # Check the cell above and subtract 2 if it's also land.
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+
+                # Check the cell to the left and subtract 2 if it's also land.
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter

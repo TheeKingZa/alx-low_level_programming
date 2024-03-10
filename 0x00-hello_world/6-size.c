4f093fec2e24bfa066028b75b66b6e2693eef943
Pule <proceedingmc@gmail.com>
2024-03-11 01:12:43 +0200

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,17 +10,16 @@
  */
 int main(void)
 {
-	// Print the size of a char
-	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	// Print the size of an int
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	// Print the size of a long int
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	// Print the size of a long long int
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	// Print the size of a float
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-	// Return 0 to indicate successful execution
-	return (0);
+// Print the size of a char
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+// Print the size of an int
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+// Print the size of a long int
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+// Print the size of a long long int
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+// Print the size of a float
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+// Return 0 to indicate successful execution
+return (0);
 }
-

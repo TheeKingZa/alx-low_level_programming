47e241b42a134224d4b8e56ae3bb76d19724073a
Pule <proceedingmc@gmail.com>
2024-03-11 01:38:18 +0200

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,8 +5,7 @@
  *
  * Description: Prints all possible combinations of two two-digit numbers,
  *              separated by a space, with each combination separated by a
- *              comma and a space.
- *              Numbers are printed with leading zeros when necessary.
+ *              comma and a space. Numbers are printed with leading zeros when necessary.
  *
  * Return: Always 0 (Success)
  */
@@ -14,24 +13,24 @@ int main(void)
 {
 int num1, num2;
 /* Iterate through all possible first two-digit numbers */
-for (num1 = 0; num1 <= 98; num1++)
+for (num1 = 0; num1 <= 99; num1++)
 {
 /* Iterate through all possible second two-digit numbers */
-for (num2 = num1 + 1; num2 <= 99; num2++)
+for (num2 = num1; num2 <= 99; num2++)
 {
 /* Print tens digit of first number */
-putchar(num1 / 10 + '0');
+putchar((num1 / 10) + '0');
 /* Print ones digit of first number */
-putchar(num1 % 10 + '0');
+putchar((num1 % 10) + '0');
 /* Print space */
 putchar(' ');
 /* Print tens digit of second number */
-putchar(num2 / 10 + '0');
+putchar((num2 / 10) + '0');
 /* Print ones digit of second number */
-putchar(num2 % 10 + '0');
+putchar((num2 % 10) + '0');
 
 /* Check if it's not the last combination */
-if (num1 != 98 || num2 != 99)
+if (!(num1 == 99 && num2 == 99))
 {
 /* Print comma and space */
 putchar(',');
@@ -42,6 +41,5 @@ putchar(' ');
 
 /* Print newline character */
 putchar('\n');
-
 return (0);
 }
